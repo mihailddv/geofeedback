2f33a367f41b36a0b33048e45ecd0277d422dd86
Дедов Михаил Александрович <Dedov.Mikhail@zolotoy.ru>
2019-06-27 12:01:51 +0300

    add form render

--- src/js/ymaps.js
+++ src/js/ymaps.js
@@ -13,24 +13,36 @@ function mapInit() {
             var coords = e.get('coords');
             var geoCoords = ymaps.geocode(coords);
             var position = e.get('position');
+
+            let pagePixels = e.get('pagePixels'),
+                reviewMain = document.querySelector('.review-main');            
         
             geoCoords.then(res => {
                 var obj = {};
+                
+                var firstGeoObject = res.geoObjects.get(0);
+                var address = firstGeoObject.properties.get('name')+", "+firstGeoObject.properties.get('description');
+                
+                let reviewAddress = document.querySelector('.review__location');
 
                 obj.coords = coords; // записываем координаты клика в объект
                 obj.address = res.geoObjects.get(0).properties.get('text'); // получаем адрес
 
-                // popup.innerHTML = render();
-
                 const htmlReview = reviewForm();
-
-                let reviewMain = document.querySelector('.review-main');
-
-                console.log(reviewMain);
 
                 reviewMain.style.display = 'block';
     
                 reviewMain.innerHTML = htmlReview;
+
+                // показывает модалку рядом с кликом
+                reviewMain.style.left = pagePixels[0]+'px';
+                reviewMain.style.top = pagePixels[1]+'px';
+
+                // адрес в заголовок формы       
+                // reviewAddress.innerText = address;
+
+                // console.log(reviewAddress);
+                
         
                 // openPopup(obj, myMap, position, clusterer, ''); // передаешь в функцию открытия модалки объект, карту, кластер (который сам создашь выше), позицию и hintContent (пока он пустой)
             });
