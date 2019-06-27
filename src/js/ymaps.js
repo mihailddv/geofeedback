9c09a9833f24711b3c607df36385caf897ce038b
Дедов Михаил Александрович <Dedov.Mikhail@zolotoy.ru>
2019-06-27 14:11:35 +0300

    add clusterer code

--- src/js/ymaps.js
+++ src/js/ymaps.js
@@ -8,6 +8,32 @@ function mapInit() {
             center: [55.75, 37.59], // Moscow
             zoom: 12
         })
+
+        // Создаем собственный макет с информацией о выбранном геообъекте.
+        var customItemContentLayout = ymaps.templateLayoutFactory.createClass(
+            // Флаг "raw" означает, что данные вставляют "как есть" без экранирования html.
+            '<h2 class=ballon_header>{{ properties.balloonContentHeader|raw }}</h2>' +
+            '<div class=ballon_body>{{ properties.balloonContentBody|raw }}</div>' +
+            '<div class=ballon_footer>{{ properties.balloonContentFooter|raw }}</div>'
+        );
+        
+        var clusterer = new ymaps.Clusterer({
+            clusterDisableClickZoom: true,
+            clusterOpenBalloonOnClick: true,
+            // Устанавливаем стандартный макет балуна кластера "Аккордеон".
+            clusterBalloonContentLayout: 'cluster#balloonAccordion',
+            // Устанавливаем собственный макет.
+            clusterBalloonItemContentLayout: customItemContentLayout,
+            // Устанавливаем режим открытия балуна. 
+            // В данном примере балун никогда не будет открываться в режиме панели.
+            clusterBalloonPanelMaxMapArea: 0,
+            // Устанавливаем размеры макета контента балуна (в пикселях).
+            clusterBalloonContentLayoutWidth: 250,
+            clusterBalloonContentLayoutHeight: 200,
+            // Можно отключить отображение иконок геообъектов в списке. 
+            // В браузере Internet Explorer ниже 9й версии иконки никогда не будут отображаться.
+            // clusterBalloonAccordionShowIcons: false
+        });
         
         myMap.events.add('click', function(e) {
             var coords = e.get('coords');
@@ -37,8 +63,6 @@ function mapInit() {
                 // показывает модалку рядом с кликом
                 reviewMain.style.left = pagePixels[0]+'px';
                 reviewMain.style.top = pagePixels[1]+'px';
-
-                console.log(obj);                
 
                 // адрес в заголовок формы       
                 // reviewAddress.innerText = address;
